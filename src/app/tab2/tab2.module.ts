68e3709d1c2420042896a7191c438080e0089e8f
gurgelrga <gurgelrga@gmail.com>
2020-09-30 22:01:46 -0300

    aula30-09 adicionado modal comida e feito form para cadastrar comida

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
@@ -12,8 +12,9 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [Tab2Page]
 })
-export class Tab2PageModule {}
+export class Tab2PageModule { }
