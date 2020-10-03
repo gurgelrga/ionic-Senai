edb66a4a1232b48821d8d8e0b97ea5b9862e086a
gurgelrga <gurgelrga@gmail.com>
2020-10-02 21:40:11 -0300

    listagem comidas, alteramos metodos de armazenamento, funcao remover

--- src/app/services/comida.service.ts
+++ src/app/services/comida.service.ts
@@ -1,38 +1,47 @@
-import { Injectable } from '@angular/core';
-import { Storage } from '@ionic/storage'
+import { Injectable } from "@angular/core";
+import { Storage } from "@ionic/storage";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ComidaService {
-
-  constructor(
-    private storage: Storage,
-  ) {
-
+  constructor(private storage: Storage) {
+    //this.removeAll();
     //storage.set("comidas", [{ nome: "Pizza" }]);
-
     // Or to get a key/value pair
     //storage.get("comidas").then((val) => {
     //  console.log("Comidasss", val);
     // });
-
   }
 
+  public async getAll() {
+    let comidas = await this.storage.get("comidas");
+    comidas = JSON.parse(comidas);
+    return comidas;
 
-  public salvarComida(comida) {
-    //console.log(">>>>>>>>", comida);
-    this.getAll().then(comidas => {
-      this.storage.set("comidas", comidas.push(comida));
-      //console.log(comidas, 'comidas');
-    });
+    //return this.storage.get("comidas").then((comidas) => {
 
+    //return Promise.resolve(comidas);
+    // });
   }
-  public getAll() {
-    return this.storage.get("comidas").then((comidas) => {
-      if (!comidas) {
-        return [];
-      }
-      return !comidas.isArray ? [] : comidas;
-    });
+  public async salvarComida(comida) {
+    let comidas = await this.getAll();
+    if (!comidas) {
+      comidas = [];
+    }
+    comidas.push(comida);
+    this.storage.set("comidas", JSON.stringify(comidas));
+    // this.getAll().then((comidas) => {
+    // comidas.push(comida);
+    // console.log(comidas);
+    // this.storage.set("comidas", comidas);
+    // });
+  }
+  public async removeAll() {
+    await this.storage.remove("comidas");
+  }
+  public async remove(key: number) {
+    let comidas = await this.getAll();
+    comidas.splice(key, 1);
+    await this.storage.set("comidas", JSON.stringify(comidas));
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,20 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
-import { ModalComidaPage } from '../modal-comida/modal-comida.page';
+import { ModalComidaPage } from "../modal-comida/modal-comida.page";
+import { ComidaService } from "../services/comida.service";
 
 @Component({
   selector: "app-tab2",
   templateUrl: "tab2.page.html",
   styleUrls: ["tab2.page.scss"],
 })
-export class Tab2Page {
-  constructor(public modal: ModalController) { }
+export class Tab2Page implements OnInit {
+  public comidas = [];
+  constructor(public modal: ModalController, public comida: ComidaService) {}
 
+  ngOnInit() {
+    this.getComidas();
+  }
   async abrirModalCadastroComida() {
     const modal = await this.modal.create({
       component: ModalComidaPage,
     });
     return await modal.present();
   }
-
+  public async getComidas() {
+    this.comidas = await this.comida.getAll();
+    console.log(this.comidas);
+  }
+  public async remover(key: number) {
+    await this.getComidas();
+    await this.comida.remove(key);
+  }
+  public async removerAll() {
+    await this.getComidas();
+    await this.comida.removeAll();
+  }
 }
